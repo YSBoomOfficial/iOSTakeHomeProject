12255236b5076fce813e14b46d74d6cc97877ae9
Yash <63893378+YSBoomOfficial@users.noreply.github.com>
2022-09-10 00:00:34 +0100

    Initial Commit

---
+++ iOSTakeHomeProject/iOSTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTakeHomeProjectApp.swift
+//  iOSTakeHomeProject
+//
+//  Created by Yash Shah on 10/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOSTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
