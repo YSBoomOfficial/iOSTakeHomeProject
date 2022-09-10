861fd3ab1d75f3f5b29c113ed0342251abdad0f2
Yash <63893378+YSBoomOfficial@users.noreply.github.com>
2022-09-10 19:33:02 +0100

    Add Form Validation to `CreateView`

--- iOSTakeHomeProject/Features/Create/ViewModels/CreateViewModel.swift
+++ iOSTakeHomeProject/Features/Create/ViewModels/CreateViewModel.swift
@@ -10,27 +10,42 @@ import Foundation
 final class CreateViewModel: ObservableObject {
 	@Published var person = NewPerson()
 	@Published private(set) var state: SubmissionState?
-	@Published private(set) var error: NetworkingManager.NetworkingError?
+	@Published private(set) var error: FormError?
 	@Published var hasError = false
 
-	func create() {
-		state = .submitting
-		let encoder = JSONEncoder()
-		encoder.keyEncodingStrategy = .convertToSnakeCase
-		let data = try? encoder.encode(person)
+	private let validator = CreateValidator()
 
-		NetworkingManager.shared.request(methodType: .POST(data: data), "https://reqres.in/api/users" + "?delay=3") { [weak self] result in
-			DispatchQueue.main.async {
-				switch result {
-					case .success():
-						self?.state = .successful
-					case let .failure(error):
-						self?.state = .unsuccessful
-						self?.hasError = true
-						self?.error = error as? NetworkingManager.NetworkingError
+	func create() {
+
+		do {
+			try validator.validate(person)
+
+			state = .submitting
+			let encoder = JSONEncoder()
+			encoder.keyEncodingStrategy = .convertToSnakeCase
+			let data = try? encoder.encode(person)
+
+			NetworkingManager.shared.request(methodType: .POST(data: data), "https://reqres.in/api/users" + "?delay=3") { [weak self] result in
+				DispatchQueue.main.async {
+					switch result {
+						case .success():
+							self?.state = .successful
+						case let .failure(error):
+							self?.state = .unsuccessful
+							self?.hasError = true
+							if let error = error as? NetworkingManager.NetworkingError {
+								self?.error = .networking(error: error)
+							}
+					}
 				}
 			}
+		} catch {
+			hasError = true
+			if let validationError = error as? CreateValidator.CreateValidatorError {
+				self.error = .validation(error: validationError)
+			}
 		}
+
 	}
 }
 
@@ -39,5 +54,19 @@ extension CreateViewModel {
 		case submitting
 		case successful
 		case unsuccessful
+	}
+}
+
+extension CreateViewModel {
+	enum FormError: LocalizedError {
+		case networking(error: LocalizedError)
+		case validation(error: LocalizedError)
+
+		var errorDescription: String? {
+			switch self {
+				case let .networking(error), let .validation(error):
+					return error.errorDescription
+			}
+		}
 	}
 }
--- iOSTakeHomeProject/Features/Create/Views/CreateView.swift
+++ iOSTakeHomeProject/Features/Create/Views/CreateView.swift
@@ -10,16 +10,24 @@ import SwiftUI
 struct CreateView: View {
 	@Environment(\.dismiss) var dismiss
 
+	@FocusState private var focusedField: Field?
 	@StateObject private var vm = CreateViewModel()
 	let successfulAction: () -> Void
-
 
 	var body: some View {
 		NavigationView {
 			Form {
-				firstName
-				lastName
-				job
+				Section {
+					firstName
+					lastName
+					job
+				} footer: {
+					if case let .validation(error) = vm.error,
+					   let errorDescription = error.errorDescription {
+						Text(errorDescription)
+							.foregroundColor(.red)
+					}
+				}
 
 				Section {
 					submit
@@ -44,11 +52,17 @@ struct CreateView: View {
 	}
 }
 
+extension CreateView {
+	enum Field: Hashable {
+		case firstName
+		case lastName
+		case job
+	}
+}
+
 struct CreateView_Previews: PreviewProvider {
 	static var previews: some View {
-		CreateView {
-
-		}
+		CreateView {}
 		.preferredColorScheme(.dark)
 	}
 }
@@ -60,17 +74,23 @@ private extension CreateView {
 
 	var firstName: some View {
 		TextField("First Name", text: $vm.person.firstName)
+			.focused($focusedField, equals: .firstName)
 	}
 
 	var lastName: some View {
 		TextField("Last Name", text: $vm.person.lastName)
+			.focused($focusedField, equals: .lastName)
 	}
 
 	var job: some View {
 		TextField("Job", text: $vm.person.job)
+			.focused($focusedField, equals: .job)
 	}
 
 	var submit: some View {
-		Button("Submit") { vm.create() }
+		Button("Submit") {
+			focusedField = nil
+			vm.create()
+		}
 	}
 }
